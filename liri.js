a27943ba66c55e27a8fb46ceb35b236c5ae965ef
Cherie Shedd <cherie.shedd@gmail.com>
2016-07-01 09:45:19 -0500

    working commit, need to tweak movie-this

--- liri.js
+++ liri.js
@@ -34,7 +34,7 @@ function getTweets(){
 }
 
 function getSong(){
-	var queryInput = "I Want it That Way";
+	var queryInput = "what's my age again";
 	if (argTwo !== undefined) {
 		queryInput = argTwo;
 	}
@@ -52,5 +52,107 @@ function getSong(){
 }
 
 function getMovie(){
-	var queryInput = ""
+	var queryInput = "Mr. Nobody";
+	if(argTwo !== undefined) {
+		queryInput = argTwo;
+	}
+request('http://www.omdbapi.com/?t=' + queryInput + "&tomatoes=true" + function(error, response, body){
+	if(!error && response.statusCode == 200){
+		var movieInfo = JSON.parse(body);
+		console.log("Title: " + movieInfo.Title);
+		console.log("Year: " + movieInfo.Year);
+		console.log("IMDB Rating: " + movieInfo.imdbRating);
+		console.log("Country: " + movieInfo.Country);
+		console.log("Language: " + movieInfo.Language);
+		console.log("Plot: " + movieInfo.Plot);
+		console.log("Actors: " + movieInfo.Actors);
+		console.log("Rotten Tomatoes Rating: " + movieInfo.tomatoesRating);
+		console.log("Rotten Tomatoes URL: " + movieInfo.tomatoesURL);
+		fs.appendFile('log.txt', "Title: " + movieInfo.Title + "\n" + "Year: " + movieInfo.Year + "\n" + "IMDB Rating: " + movieInfo.imdbRating + "\n" + "Country: " + movieInfo.Country + "\n" + "Language: " + movieInfo.Language + "\n" + "Plot: " + movieInfo.Plot + "\n" + "Actors: " + movieInfo.Actors + "\n" + "Rotten Tomatoes Rating: " + movieInfo.tomatoesRating + "\n" + "Rotten Tomatoes URL: " + movieInfo.tomatoesURL);
+	}
+	else {
+		console.log(error);
+	}
+	});
 }
+
+function getRandom() {
+	fs.readFile("random.txt", "utf8", function(error, data){
+		//show if error occurs
+		if(error){
+			console.log(error);
+		} else {
+			var dataArray = data.split(',');
+			var argOne = dataArray[0];
+			var argTwo = dataArray[1];
+			switch(argOne) {
+				case "my-tweets":
+				getTweets();
+				break;
+				case "spotify-this-song":
+					function getSong(){
+						var queryInput = "what's my age again";
+						if (argTwo !== undefined) {
+							queryInput = argTwo;
+						}
+						spotify.search({ type: 'track', query: queryInput, count: 1}, function(err, data){
+							if(err){
+								console.log('Error: ' + err);
+								return;
+							}
+							console.log("Artist: " + data.tracks.items[0].artists[0].name);
+							console.log("Song: " + data.tracks.items[0].name);
+							console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
+							console.log("Album: " + data.tracks.items[0].album.name);
+							fs.appendFile('log.txt', "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "Song: " + data.tracks.items[0].name + "\n" + "Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify + "\n" + "Album: " + data.tracks.items[0].album.name); 
+						});
+
+					}
+					getSong();
+					break;
+					case "movie-this":
+						function getMovie(){
+							var queryInput = "Mr.Nobody";
+							if(argTwo !== undefined) {
+								queryInput = argTwo;
+							}
+							request('http://www.omdbapi.com/?t=' + queryInput + "&tomatoes=true" + function(error, response, body){
+								if(!error && response.statusCode == 200){
+									var movieInfo = JSON.parse(body);
+									console.log("Title: " + movieInfo.Title);
+									console.log("Year: " + movieInfo.Year);
+									console.log("IMDB Rating: " + movieInfo.imdbRating);
+									console.log("Country: " + movieInfo.Country);
+									console.log("Language: " + movieInfo.Language);
+									console.log("Plot: " + movieInfo.Plot);
+									console.log("Actors: " + movieInfo.Actors);
+									console.log("Rotten Tomatoes Rating: " + movieInfo.tomatoesRating);
+									console.log("Rotten Tomatoes URL: " + movieInfo.tomatoesURL);
+									fs.appendFile('log.txt', "Title: " + movieInfo.Title + "\n" + "Year: " + movieInfo.Year + "\n" + "IMDB Rating: " + movieInfo.imdbRating + "\n" + "Country: " + movieInfo.Country + "\n" + "Language: " + movieInfo.Language + "\n" + "Plot: " + movieInfo.Plot + "\n" + "Actors: " + movieInfo.Actors + "\n" + "Rotten Tomatoes Rating: " + movieInfo.tomatoesRating + "\n" + "Rotten Tomatoes URL: " + movieInfo.tomatoesURL);
+								}
+									else {
+									console.log(error);
+								}
+							});
+						}
+						getMovie();
+						break;
+				}
+			}
+		});
+	}
+
+switch(argOne){
+	case "my-tweets":
+	getTweets();
+	break;
+	case "spotify-this-song":
+	getSong();
+	break;
+	case "movie-this":
+	getMovie();
+	break;
+	case "do-what-it-says":
+	getRandom();
+	break;
+}	
